c589de416f244a3c490fe79faec51bc024c903f8
Fabian Mäkilä <contact@fabianadrian.fi>
2025-03-11 16:23:10 +0200

    add examples and comments to the default configuration

--- src/main/java/fi/fabianadrian/playerlist/configuration/Configuration.java
+++ src/main/java/fi/fabianadrian/playerlist/configuration/Configuration.java
@@ -3,13 +3,24 @@ package fi.fabianadrian.playerlist.configuration;
 import fi.fabianadrian.playerlist.list.sorting.LuckPermsSorter;
 import fi.fabianadrian.playerlist.list.sorting.Sorter;
 import fi.fabianadrian.playerlist.list.sorting.SortingOrder;
+import org.spongepowered.configurate.objectmapping.meta.Comment;
 
 import java.util.List;
 
 public class Configuration {
-	private List<String> header = List.of();
-	private List<String> footer = List.of();
+	private List<String> header = List.of(
+			"<blue>This is an example",
+			"<red>header with 2 lines"
+	);
+	private List<String> footer = List.of(
+			"<rainbow>This is a footer"
+	);
+	@Comment("""
+			The example uses MiniPlaceholders to display a prefix.
+			Set to "" to use the vanilla name.
+			""")
 	private String playerListName = "<luckperms_prefix><player_name>";
+	@Comment("Please refer to the wiki on how to use the sorters.")
 	private List<Sorter> sorters = List.of(
 			new LuckPermsSorter(SortingOrder.DESCENDING, LuckPermsSorter.Criteria.PREFIX_WEIGHT)
 	);
